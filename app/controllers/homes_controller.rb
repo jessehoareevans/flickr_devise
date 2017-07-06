ba3c69f2c8e04ac51e20da61606f8aed7db0d0a0
Ben Petrosky and Jesse Hoare Evans <ben+jesse@>
2017-07-06 09:32:10 -0700

    root route

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,8 @@
+class HomesController < ApplicationController
+
+
+def index
+
+render :index
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
 
-    root :to => 'registrations#new'
 
-    get '/new_user_session' => 'devise/sessions#new'
-    get '/new_user_password' => 'devise/password#new'
-    get '/edit_user_password' => 'devise/passwords#edit'
-    get '/cancel_user_registration' => 'devise/registrations#cancel'
-    get '/new_user_registration' => 'devise/registrations#new'
-    get '/edit_user_registration' => 'devise/registrations#edit'
-    get '/signup' => 'users#new'
-    get '/signin' => 'sessions#new'
-    get '/signout' => 'sessions#destroy'
+  root 'homes#index'
+  devise_for :users, controllers: {
 
-    post '/user_session' => 'devise/sessions#create'
-    post '/registrations' => 'registrations#create'
+  sessions: 'users/sessions'
+
+}
+
+
+
 
 end
